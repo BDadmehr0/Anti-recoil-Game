a2e2d8e5c3df7a48c939bb253a155568cd8a0440
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-05-22 02:24:31 +0430

    panel

--- main.py
+++ main.py
@@ -17,7 +17,26 @@ class Sound:
         
 root = CTk()
 root.geometry('550x400')
+root.resizable(width=False,height=False)
 root.title('Css Cheat Crysis')
 root.iconbitmap('lannister_115487 (min).ico')
+
+Title_lb = CTkLabel(root,text='CSS Cheat | Crysis',font=('Roboto',24))
+Title_lb.pack(padx=12,pady=10)
+
+opttsions = CTkFrame(root)
+opttsions.pack(padx=23,pady=20)
+
+
+BHOP_btn = CTkButton(opttsions,text='Bhop',font=('bold',15))
+BHOP_btn.pack(padx=12,pady=10)
+
+no_rec_btn = CTkButton(opttsions,text='No Recoil',font=('bold',15))
+no_rec_btn.pack(padx=12,pady=10)
+
+
+auto_chat_btn = CTkButton(opttsions,text='Auto Chat',font=('bold',15))
+auto_chat_btn.pack(padx=12,pady=10)
+
 
 root.mainloop()
